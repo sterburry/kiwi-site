a5c795387aaf5dc43f72479d1f331ece5fd9aaf6
sterburry <privatemoments.78@gmail.com>
2026-04-07 19:37:42 -0400

    Add YouTube login handler for OAuth2 authentication

---
+++ api/youtube-login.js
@@ -0,0 +1,20 @@
+export default function handler(req, res) {
+  const client_id = process.env.YOUTUBE_CLIENT_ID;
+  const redirect_uri = process.env.YOUTUBE_REDIRECT_URI;
+
+  const scope = [
+    "https://www.googleapis.com/auth/youtube",
+    "https://www.googleapis.com/auth/youtube.force-ssl"
+  ].join(" ");
+
+  const authUrl =
+    `https://accounts.google.com/o/oauth2/v2/auth` +
+    `?client_id=${client_id}` +
+    `&redirect_uri=${encodeURIComponent(redirect_uri)}` +
+    `&response_type=code` +
+    `&access_type=offline` +
+    `&prompt=consent` +
+    `&scope=${encodeURIComponent(scope)}`;
+
+  res.redirect(authUrl);
+}
