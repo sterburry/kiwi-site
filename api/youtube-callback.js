4185362cb11c5f95496634470488afceb532143f
sterburry <privatemoments.78@gmail.com>
2026-04-07 19:38:17 -0400

    Add YouTube OAuth callback handler

---
+++ api/youtube-callback.js
@@ -0,0 +1,27 @@
+export default async function handler(req, res) {
+  const code = req.query.code;
+
+  const client_id = process.env.YOUTUBE_CLIENT_ID;
+  const client_secret = process.env.YOUTUBE_CLIENT_SECRET;
+  const redirect_uri = process.env.YOUTUBE_REDIRECT_URI;
+
+  const tokenRes = await fetch("https://oauth2.googleapis.com/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: new URLSearchParams({
+      code,
+      client_id,
+      client_secret,
+      redirect_uri,
+      grant_type: "authorization_code"
+    })
+  });
+
+  const data = await tokenRes.json();
+
+  // ⚠️ For now we just redirect with token in URL (simple version)
+  // Later we store properly in DB
+  res.redirect(`/?access_token=${data.access_token}`);
+}
