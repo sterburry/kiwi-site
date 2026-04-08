eae591ae0ad271cc5c4c2133f94886e320f4764f
sterburry <privatemoments.78@gmail.com>
2026-04-08 17:42:23 -0400

    Update now-playing.js

--- api/now-playing.js
+++ api/now-playing.js
@@ -2,9 +2,7 @@ export default async function handler(req, res) {
   const client_id = process.env.SPOTIFY_CLIENT_ID;
   const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
   const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
-
   const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
-
   const tokenResponse = await fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
     headers: {
@@ -16,10 +14,8 @@ export default async function handler(req, res) {
       refresh_token
     })
   });
-
   const tokenData = await tokenResponse.json();
   const access_token = tokenData.access_token;
-
   const nowPlaying = await fetch(
     "https://api.spotify.com/v1/me/player/currently-playing",
     {
@@ -28,32 +24,26 @@ export default async function handler(req, res) {
       }
     }
   );
-
   if (nowPlaying.status === 204 || nowPlaying.status > 400) {
     return res.status(200).json({ isPlaying: false });
   }
-
   const song = await nowPlaying.json();
-
-  // 🎧 GET ARTIST ID
   const artistId = song.item.artists[0].id;
-
-  // 🎧 FETCH ARTIST DATA (THIS CONTAINS GENRES)
   const artistRes = await fetch(`https://api.spotify.com/v1/artists/${artistId}`, {
     headers: {
       Authorization: `Bearer ${access_token}`,
     },
   });
-
   const artistData = await artistRes.json();
   const genres = artistData.genres || [];
-
   return res.status(200).json({
     title: song.item.name,
     artist: song.item.artists.map(a => a.name).join(", "),
     albumImage: song.item.album.images[0].url,
     songUrl: song.item.external_urls.spotify,
     isPlaying: true,
-    genres: genres
+    genres: genres,
+    progress: song.progress_ms,
+    duration: song.item.duration_ms
   });
 }
